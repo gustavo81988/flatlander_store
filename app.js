9d22a669d05f1fdfe3a1fd1771628ed866902a93
Gustavo Ramirez <gustavo.ramirez@ingeniandoweb.com>
2016-01-12 20:09:57 -0430

    Accepting Submissions

--- app.js
+++ app.js
@@ -6,6 +6,16 @@
 		this.products = gems;
 	});
 
+	app.controller('ReviewController',function(){
+		this.review = {};
+
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+
+	});
+
 	app.controller('PanelController',function(){
 		
 		this.tab = 3 ;
